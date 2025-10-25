1c8a2f78ed2469778c0af0928f41ba3b69438021
mbsomeday <137542994@qq.com>
2025-10-25 17:37:03 +0100

    update

--- data/dataset.py
+++ data/dataset.py
@@ -30,8 +30,8 @@ class my_dataset(Dataset):
         self.txt_name = txt_name
         self.img_transforms = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize(mean=(0.485, 0.456, 0.406),
-                                 std=(0.229, 0.224, 0.225))
+            # transforms.Normalize(mean=(0.485, 0.456, 0.406),
+            #                      std=(0.229, 0.224, 0.225))
         ])
 
         self.images, self.ped_labels, self.ds_labels = self.init_ImagesLabels()
