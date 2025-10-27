63b785fd4f2f7e2553d4dc3956e6c64833b6453c
mbsomeday <137542994@qq.com>
2025-10-27 15:57:50 +0000

    update

--- scripts/train_pedestrianClassifier.py
+++ scripts/train_pedestrianClassifier.py
@@ -22,7 +22,7 @@ def get_args():
     # train
     parser.add_argument('--base_lr', type=float, default=0.01)
     parser.add_argument('--isTrain', action='store_true')
-    parser.add_argument('--min_train_epoch', type=int, default=15)
+    parser.add_argument('--min_train_epoch', type=int, default=10)
     parser.add_argument('--max_train_epoch', type=int, default=50)
     parser.add_argument('--seed_num', type=int, default=1)
     parser.add_argument('--rand_seed', type=int, default=13)
