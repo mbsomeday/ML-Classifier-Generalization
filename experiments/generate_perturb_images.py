cbde357d5010df9254985b6ba8a72fb9a1cbfea9
mbsomeday <137542994@qq.com>
2026-01-23 13:29:57 +0000

    update

--- experiments/generate_perturb_images.py
+++ experiments/generate_perturb_images.py
@@ -128,10 +128,12 @@ def gen_part_aug(opts):
         plt_mask = cam_mask * 1.0
 
         plt_mask = plt_mask[np.newaxis, :]
+        plt_mask.to(DEVICE)
 
         # 加载perturb图片
         perturb_image_path = os.path.join(opts.perturb_dir, cls_name, img_name)
         perturb_image = Image.open(perturb_image_path).convert('RGB')
+        perturb_image.to(DEVICE)
 
         # 进行的图片扩增
         # 这里要注意，如果是flip和rotate，需要先将perturbation与org image结合，然后再进行aug操作，否则，CAM对应的位置会变
@@ -141,14 +143,14 @@ def gen_part_aug(opts):
 
         # aug操作为flip和rotate，
         if random_aug_id == 0 or random_aug_id == 1:
-            perturb_and_org = transforms.ToTensor()(perturb_image) * (1 - plt_mask) + image[0].cpu.numpy() * plt_mask
+            perturb_and_org = transforms.ToTensor()(perturb_image) * (1 - plt_mask) + image[0] * plt_mask
             perturb_and_aug = cur_aug_operation(perturb_and_org)
 
         else:
             # 直接将perturb与org图片合并
             aug_image = cur_aug_operation(plt_transformer(image[0]))
             aug_image_tensor = tensor_transformer(aug_image)
-            perturb_and_aug = transforms.ToTensor()(perturb_image) * (1 - plt_mask) + aug_image_tensor.cpu.numpy() * plt_mask
+            perturb_and_aug = transforms.ToTensor()(perturb_image) * (1 - plt_mask) + aug_image_tensor * plt_mask
 
         save_perturbAug_name = os.path.splitext(img_name)[0] + '_perturb' + aug_name_list[random_aug_id] + os.path.splitext(img_name)[-1]
         save_perturbAug_path = os.path.join(opts.perturbAug_save_dir, cls_name, save_perturbAug_name)
