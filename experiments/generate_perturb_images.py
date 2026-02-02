7c0345acfd19eb37802e4430f5b8537ef3695eca
mbsomeday <137542994@qq.com>
2026-02-02 13:38:13 +0000

    update

--- experiments/generate_perturb_images.py
+++ experiments/generate_perturb_images.py
@@ -33,25 +33,44 @@ from utils.utils import load_model, DEVICE, save_image_tensor
 def get_opts():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--task', type=str, choices=['perturbed', 'cmbined'], default='perturb')
-    parser.add_argument('--ds_name_list', nargs='+', default=['D2'])
-    parser.add_argument('--txt_name', type=str, default='train.txt')
-    parser.add_argument('--num_classes', type=int, default=3, help='the number is 3 when using dataset classifier, and is 2 when using pedestrian classifier')
-    parser.add_argument('--model_weights', type=str, default=r'D:\my_phd\Model_Weights\Stage6\new_dataset\dsClsD1D2D3-08-1.09839.pth')
+    # 用于云端
+    parser.add_argument('--task', type=str, choices=['perturbed', 'cmbined'], required=True, default=None)
+    parser.add_argument('--ds_name_list', nargs='+')
+    parser.add_argument('--txt_name', type=str)
+    parser.add_argument('--num_classes', type=int, help='the number is 3 when using dataset classifier, and is 2 when using pedestrian classifier')
+    parser.add_argument('--model_weights', type=str)
 
-    # parser.add_argument('--model_weights', type=str, default=r'D:\my_phd\Model_Weights\Stage6\new_dataset\baselines\D2\efficientNetB0_D2_51_Baseline-19-2.00064.pth')
-
-    # parser.add_argument('--ds_weights_path', type=str, default=r'D:\my_phd\Model_Weights\Stage6\new_dataset\dsClsD1D2D3-08-1.09839.pth')
-    parser.add_argument('--path_key', type=str, default='Stage6_org')
-    parser.add_argument('--batch_size', type=int, default=1)
+    parser.add_argument('--path_key', type=str)
+    parser.add_argument('--batch_size', type=int)
 
     # 生成perturbation图片
-    parser.add_argument('--perturb_save_dir', type=str, default=None)
+    parser.add_argument('--perturb_save_dir', type=str)
 
     # 生成perturbation+aug图片
-    parser.add_argument('--perturb_dir', type=str, default=r'D:\my_phd\dataset\Stage6\stage6_citypersons\Processor\M2CAM\onlyPurturbations\train')
-    # parser.add_argument('--perturb_dir', type=str, default=None)
-    parser.add_argument('--compondImg_save_dir', type=str, default=None)
+    parser.add_argument('--perturb_dir', type=str)
+    parser.add_argument('--compondImg_save_dir', type=str)
+
+
+    # # 用于本地测试
+    # parser.add_argument('--task', type=str, choices=['perturbed', 'cmbined'], default='perturb')
+    # parser.add_argument('--ds_name_list', nargs='+', default=['D2'])
+    # parser.add_argument('--txt_name', type=str, default='train.txt')
+    # parser.add_argument('--num_classes', type=int, default=3, help='the number is 3 when using dataset classifier, and is 2 when using pedestrian classifier')
+    # parser.add_argument('--model_weights', type=str, default=r'D:\my_phd\Model_Weights\Stage6\new_dataset\dsClsD1D2D3-08-1.09839.pth')
+    #
+    # # parser.add_argument('--model_weights', type=str, default=r'D:\my_phd\Model_Weights\Stage6\new_dataset\baselines\D2\efficientNetB0_D2_51_Baseline-19-2.00064.pth')
+    #
+    # # parser.add_argument('--ds_weights_path', type=str, default=r'D:\my_phd\Model_Weights\Stage6\new_dataset\dsClsD1D2D3-08-1.09839.pth')
+    # parser.add_argument('--path_key', type=str, default='Stage6_org')
+    # parser.add_argument('--batch_size', type=int, default=1)
+    #
+    # # 生成perturbation图片
+    # parser.add_argument('--perturb_save_dir', type=str, default=None)
+    #
+    # # 生成perturbation+aug图片
+    # parser.add_argument('--perturb_dir', type=str, default=r'D:\my_phd\dataset\Stage6\stage6_citypersons\Processor\M2CAM\onlyPurturbations\train')
+    # # parser.add_argument('--perturb_dir', type=str, default=None)
+    # parser.add_argument('--compondImg_save_dir', type=str, default=None)
 
     opts = parser.parse_args()
 
