871fe9d0cdcd86d858c6db3eae571fabf8b8f6cf
mbsomeday <137542994@qq.com>
2026-04-30 17:12:28 +0100

    update

--- experiment_classes/operated_datasets.py
+++ experiment_classes/operated_datasets.py
@@ -342,7 +342,7 @@ def gen_PerturbAug_AugPerturb(opts):
             onlyPerturb_image = Image.open(onlyPerturb_image_dir).convert('RGB')
             mask_image = Image.open(mask_image_path).convert("L")
             mask_image = np.array(mask_image)
-            mask_image = (mask_image > 0).astype(np.uint8)
+            mask_image = (mask_image > 0).astype(np.uint8).to(DEVICE)
 
             for opt_idx, opt in enumerate(operation_list):
                 if opt == 'AugPerturb':
@@ -453,7 +453,7 @@ def gen_AugOrg_OrgAug(opts):
             mask_image_path = os.path.join(mask_dir, cls_name, img_name)
             mask_image = Image.open(mask_image_path).convert("L")
             mask_image = np.array(mask_image)
-            mask_image = (mask_image > 0).astype(np.uint8)
+            mask_image = (mask_image > 0).astype(np.uint8).to(DEVICE)
 
             for opt_idx, opt in enumerate(operation_list):
                 if opt == 'OrgAug':
