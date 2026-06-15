c70d8434c2e36a808f17f3e2c5990ef570179dc4
mbsomeday <137542994@qq.com>
2026-06-15 09:57:27 +0100

    update

--- experiment_classes/pedestrian_classification.py
+++ experiment_classes/pedestrian_classification.py
@@ -24,8 +24,8 @@ class Ped_Classifier():
                 print(f'Running on {torch.cuda.get_device_name(0)} GPU')
 
         self.opts = opts
-        self.ped_model = get_obj_from_str(self.opts.ped_model_obj)(num_classes=2).to(DEVICE)
-        # self.ped_model = timm.create_model("deit_small_patch16_224", pretrained=False, num_classes=2).to(DEVICE)
+        # self.ped_model = get_obj_from_str(self.opts.ped_model_obj)(num_classes=2).to(DEVICE)
+        self.ped_model = timm.create_model("deit_small_patch16_224", pretrained=False, num_classes=2).to(DEVICE)
 
         if self.opts.isTrain:
             self.training_setup()
