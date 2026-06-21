2dba68854ea1f921da0e33a39c44b11fe21ee7c9
mbsomeday <137542994@qq.com>
2026-06-21 15:59:04 +0100

    update

--- scripts/gen_operated_group_data/gen_operated_datasets.py
+++ scripts/gen_operated_group_data/gen_operated_datasets.py
@@ -1,8 +1,9 @@
 # 将上级目录加入 sys.path， 防止命令行运行时找不到包
 import os, sys
 curPath = os.path.abspath(os.path.dirname(__file__))
-root_path = os.path.split(curPath)[0]
+root_path = os.path.split(curPath)[2]
 sys.path.append(root_path)
+
 from experiment_classes.operated_datasets import GEN_FUNC_REGISTRY
 
 
